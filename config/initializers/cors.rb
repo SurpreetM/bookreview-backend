17b2bfe5a547729c507c97b2645af9b7acd6ec5a
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-05-11 09:10:50 -0400

    adding in the rack-cors gem into the file to support the resource sharing between our API and the front end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+ Rails.application.config.middleware.insert_before 0, Rack::Cors do
+   allow do
+     origins '*'
+    # origins is the server that the front end is running on. Note we could alternatively specify this (e.g. localhost:3001)
+    # having a * here means any origin can make a request to this API
+    # the request can have access to any of the methods listeed below
+     resource '*',
+       headers: :any,
+       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+   end
+ end
