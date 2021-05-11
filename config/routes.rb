4ca33aef9a8de504a02e0cdee55b097443698d3a
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-05-11 09:24:47 -0400

    adding namespacing to make clear this app is an api only

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :books
+  namespace :api do
+    namespace :v1 do
+      resources :reviews
+      resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
+  end
 end
