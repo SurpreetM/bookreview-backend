7f8dbe98a7b14f65dbe6a3051258b32c838b1cca
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-05-14 10:19:33 -0400

    adding book id to the book serializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,5 +1,5 @@
 class BookSerializer < ActiveModel::Serializer
   # include FastJsonapi::ObjectSerializer
-  attributes :title, :author, :genre, :average_rating
+  attributes :title, :author, :genre, :average_rating, :id
   has_many :reviews
 end
