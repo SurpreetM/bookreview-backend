4ac5937849b9bd2cfc52ce6bb530d51b2028e58b
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-05-19 10:12:53 -0400

    Including the review id in the review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
   # include FastJsonapi::ObjectSerializer
-  attributes :rating, :comments, :book_id
+  attributes :rating, :comments, :book_id, :id
 end
