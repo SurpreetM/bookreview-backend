ccc84875786d0a91551fa11e28523a74f1d6d687
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-05-22 11:09:49 -0400

    minor edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-book_one = Book.create(title: 'Harry potter and the philosophers stone', author: 'JK Rowlings', genre: 'Fantasy', average_rating: 10)
+book_one = Book.create(title: 'Harry potter and the philosophers stone', author: 'JK Rowlings', genre: 'Fantasy', average_rating: 8)
 
 review_one = Review.create(rating: 10, comments: 'One of my favorites! I could read it over and over again!', book_id: book_one.id)
+
+review_two = Review.create(rating: 10, comments: 'This book is full of mystery, adventure, and multiple plot twists!', book_id: book_one.id)
+
+review_three = Review.create(rating: 4, comments: "If you aren't keen on today's commercialized version of Halloween, you won't enjoy the Harry Potter stories either", book_id: book_one.id)
+
+book_two = Book.create(title: "Sapiens: A Brief History of Humankind", author: 'Yuval Noah Harari', genre: 'Non-Fiction', average_rating: 0)
